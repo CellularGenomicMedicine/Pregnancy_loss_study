77bbd18dbf952541f3cda74244d1366f3aa29b4b
RickEssers <94915337+RickEssersMUMC@users.noreply.github.com>
2023-05-24 10:55:23 +0200

    Add files via upload

--- 1_DataConversion/Run3/DataConversion_Colnames_Run3.R
+++ 1_DataConversion/Run3/DataConversion_Colnames_Run3.R
@@ -2,35 +2,26 @@
 # Author: Rick Essers
 # Lab: Cellular Genomic Medicine, Clinical Genetics, Maastricht University Medical Center (MUMC+)
 
-# script purpose: To convert SNP genotyping raw data into data that is compatible with haplarithmisis. To extract individual family data files from the raw SNP genotyping data file.
+# script purpose: To convert SNP genotyping raw data into data that is compatible with haplarithmisis.
 
-# input: Raw SNP genotyping data from the 3rd run (out of 3 total), available on GEO (GSE228151).
+# input: .adj file containing SNP genotyping data for each individual pregnancy loss family. 
 
 # output: Data files for individual pregnancy loss families that are compatible with haplarithmisis (format: .adj). 
 
 ###########################################################################################################################
 
-dataPath<- "/Projects/PregnancyLoss/Data/Raw/"
-outPath <- "/Projects/PregnancyLoss/Data/Processed/"
+args <- commandArgs(TRUE)
+Family <- args[1]
 
-data <- read.table(paste0(dataPath,"MiscarrSamples_2022_02_14.txt"),sep="\t",header=T,nrow=1)
-dataAll <- read.table(paste0(dataPath,"MiscarrSamples_2022_02_14.txt"),sep="\t",header=T)
+dataPath <- "/ifs/data/research/CGM/Projects/Miscarr/2022/Families/FamsRound3/Test/"
 
-Cols <- gsub(".Log.R.Ratio","",colnames(data))
-Cols <- gsub(".GType","",Cols)
-Cols <- gsub(".B.Allele.Freq","",Cols)
+dataPL <- read.table(paste0(dataPath,Family),sep="\t",header=T)
 
-Cols <- gsub("father","",Cols)
-Cols <- gsub("mother","",Cols)
-Cols <- gsub("chorionicvilli","",Cols)
-Cols <- gsub("mesoderma","",Cols)
-Cols <- gsub("chorionic.villi","",Cols)
+colnames(dataPL) <- gsub("father","Father",colnames(dataPL))
+colnames(dataPL) <- gsub("mother","Mother",colnames(dataPL))
+colnames(dataPL) <- gsub("chorionicvilli","Affected",colnames(dataPL))
+colnames(dataPL) <- gsub("mesoderma","E01_Bl01",colnames(dataPL))
+colnames(dataPL) <- gsub(Family,"",colnames(dataPL))
 
-Fams <- unique(do.call("rbind",strsplit(Cols,"_"))[,1])[-c(1:3)]
+write.table(dataPL,paste0(dataPath,Family),col.names=T,row.names=F,quote=F,sep="\t")
 
-for(f in Fams){ 
-  dataOut<- dataAll[,c(1:3,grep(f,colnames(data)))]
-  
-  print(f)
-  write.table(dataOut, paste0(outPath,f,".adj"),col.names=T,row.names=F,quote=F,sep="\t") 
-}
