b213a5236412abeb09fdac51b3ce3d3c705628f7
RickEssers <94915337+RickEssersMUMC@users.noreply.github.com>
2023-05-25 15:31:30 +0200

    Add files via upload

--- 5_DataAnalysis_Plotting_Statistics/functions/Indication_colors.R
+++ 5_DataAnalysis_Plotting_Statistics/functions/Indication_colors.R
@@ -12,19 +12,30 @@ color_Aut_mon   <- "#FFCC33"
   
 color_Abnormal                <- "#F8766D"
 color_Normal                  <- "#00BFC4"
-color_RPL                     <- "#DAA520"
-color_SPL                     <- "#6495ED"
+#color_RPL                    <- "#DAA520"
+#color_SPL                    <- "#6495ED"
   
-color_Meiotic_II              <- "#333300"
-color_Meiotic                 <- "#048F45" #Or Meiosis I 
-color_Mitotic                 <- "#0CB702"
-color_Mitotic_and_meiotic     <- "#92D895"
+color_Meiotic_II             <-             "#003366"   #"#333300"
+color_Meiotic                 <- "#274093"   #"#003366"   #"#3182BD"  #"#048F45" #Or Meiosis I 
+color_Mitotic                 <- "#3182BD"   #"#9ECAE1"   #"#0CB702"
+color_Mitotic_and_meiotic     <- "#9ECAE1"   #"#DEEBF7"   #"#92D895"
   
 color_Paternal                <- "#F37735"
 color_Maternal                <- "#C77CFF"
 color_Maternal_and_Paternal   <- "#993300"
   
 color_NA                      <- "#8C8C8C"
+
+color_CV                      <- "#799141"
+color_EM                      <- "#E67A74"
+
+color_Gain                    <- "#993300"     
+color_Neutral                 <- "#996600"      
+color_Loss                    <- "#CC9933"      
+
+color_Segmental               <- "#FF33CC"
+color_Chromosomal             <- "#990099"
+color_GenomeWide              <- "#5331A3"   #"#660066"
 
 #SNPHapla                     <- "#660000" OR "#6495ED"
 #ConvKaryo                    <- "#CD9600" OR "#DAA520"
